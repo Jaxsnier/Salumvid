8a142411a6b97ac3c18b7587c3c10055be4fa5bc
ELIGIO <eligioestevez@hotmail.com>
2025-09-04 18:45:15 -0400

    expor modulo app

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    console.log(port);
     res.send('Hello World! funcionando');
 });
 
 app.listen(port, () => {
     console.log('Server is running on ${port}');
+        console.log(port);
 });
+
+module.exports = app;
+
