1f85aa7da909bf0d5576f7a7814450bb1a45bafc
ELIGIO <eligioestevez@hotmail.com>
2025-09-04 18:33:36 -0400

    configurando vercel

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World! funcionando');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(port, () => {
+    console.log('Server is running on ${port}');
 });
