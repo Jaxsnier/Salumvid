175ca9cc4d301c9876b69756c2b66902a6d8839b
ELIGIO <eligioestevez@hotmail.com>
2025-09-14 10:23:38 -0400

    agregando filas a la tabla

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ let ruta = 'login'; //registro o home
 let urlogin = 'https://login-ten-sigma-39.vercel.app/auth/login';
 let urlme = 'https://login-ten-sigma-39.vercel.app/auth/me';
 
+var ventanas = [];
+
 let token = localStorage.getItem('token');
 
 const renderApp = () => {
@@ -14,7 +16,7 @@ const renderApp = () => {
 const renderHome = () => {
     const homeTemplate = document.getElementById('home-template');
     document.getElementById('app').innerHTML = homeTemplate.innerHTML;
-    
+
     const btnAgregarVentana = document.getElementById('btnAgregarVentana'); // Botón "Me Informacion"
     btnAgregarVentana.addEventListener('click', AgregarVentana);
 }
@@ -47,21 +49,87 @@ const renderLogin = () => {
     })
 }
 const AgregarVentana = () => {
-    alert('Funcionalidad en desarrollo.');
+
+    var medidas = [                                 //falta comprobar que no faltan datos
+        document.getElementById("ventana-cantidad").value,
+        document.getElementById("ventana-ancho").value,
+        document.getElementById("ventana-alto").value,]
+
+    ventanas.push({
+        // Medidas
+        id: ventanas.length + 1,
+        cantidad: medidas[0],
+        ancho: medidas[1],
+        alto: medidas[2],
+        hojas: -1,
+        //Perfiles Marco
+        Cant: -1,
+        Medida: -1,
+        Cant: -1,
+        Medida: -1,
+        // Perfiles Hoja
+        Cant: -1,
+        Medida: -1,
+        Cant: -1,
+        Medida: -1,
+        // Vidrio
+        Cant: -1,
+        Ancho: -1,
+        Alto: -1,
+        // Goma
+        Pie: -1,
+
+    });
+    
+    //console.log(ventanas)
+    
+    RenderVentana(ventanas[ventanas.length-1]);  //renderizamos todas las ventanas //luego implementar renderizar solo la ultima
 }
 const InfoMe = () => {
     return fetch(urlme, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: token,
-            }
-        }).then(response => response.json()).then(User => {
-            alert(`User Info:\nName: ${User.username}\nOtros datos: ${JSON.stringify(User)}`);
-        }).catch(error => {
-            alert('Failed to fetch user information.' + token);
-        });   
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: token,
+        }
+    }).then(response => response.json()).then(User => {
+        alert(`User Info:\nName: ${User.username}\nOtros datos: ${JSON.stringify(User)}`);
+    }).catch(error => {
+        alert('Failed to fetch user information.' + token);
+    });
 }
+
+const RenderVentana = (ventana) =>
+    {
+        tbody = document.getElementById("contenido-tabla"); //buscamos contenido actual de la tabla
+
+        const row = `
+            <tr>
+                <td>${ventana.id}</td>
+                <td>${ventana.cantidad ?? ''}</td>
+                <td>${ventana.ancho ?? ''}</td>
+                <td>${ventana.alto ?? ''}</td>
+                <td>${ventana.hojas ?? ''}</td>
+
+                <td>${ventana.marcoLateralesCant ?? ''}</td>
+                <td>${ventana.marcoLateralesMed ?? ''}</td>
+                <td>${ventana.marcoRielCant ?? ''}</td>
+                <td>${ventana.marcoRielMed ?? ''}</td>
+
+                <td>${ventana.hojaCabCant ?? ''}</td>
+                <td>${ventana.hojaCabMed ?? ''}</td>
+                <td>${ventana.hojaJambaCant ?? ''}</td>
+                <td>${ventana.hojaJambaMed ?? ''}</td>
+
+                <td>${ventana.vidrioCant ?? ''}</td>
+                <td>${ventana.vidrioAncho ?? ''}</td>
+                <td>${ventana.vidrioAlto ?? ''}</td>
+
+                <td>${ventana.Pie ?? ''}</td>
+            </tr>
+        `;
+        tbody.insertAdjacentHTML('beforeend', row);
+    }
 
 window.onload = () => {
     renderApp();
